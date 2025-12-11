85e1d767513ac34b35942a74372e8affa06b5f11
Kuruz253 <lilka25531@gmail.com>
2025-12-11 20:15:45 +0100

    main.py

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
-meme_dict = {
-    'LOL': 'odpowiedź na coś zabawnego',
-    'CRINGE': 'coś dziwnego lub wstydliwego',
-    'ROFL': 'odpowiedź na żart',
-    'SHEESH': 'lekka dezaprobata',
-    'CREEPY': 'straszny, złowieszczy',
-    'AGGRO': 'stać się agresywnym/zły'
-}
-print('Wszystkie slowa', meme_dict)
+import random
+import discord
+from discord.ext import commands
+from bot_logic import gen_pass
+# Zmienna intents przechowuje uprawnienia bota
+intents = discord.Intents.default()
+# Włączanie uprawnienia do czytania wiadomości
+intents.message_content = True
+# Tworzenie bota w zmiennej client i przekazanie mu uprawnień
+bot = commands.Bot(command_prefix='$', intents=intents)
+@bot.event
+async def on_ready():
+    print(f'Zalogowaliśmy się jako {bot.user}')
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Cześć, jestem bot {bot.user}")
+@bot.command()
+async def haslo(ctx):
+    await ctx.send(gen_pass(10))
 
-word = input("Wpisz słowo, którego nie rozumiesz (używaj wielkich liter!): ")
+@bot.command()
+async def roll(ctx, dice: str):
+    
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
 
-if word in meme_dict.keys():
-    print(meme_dict[word])
-else:
-    print("Nie mamy takiego słowa")
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+
+
+
+bot.run("Token")
